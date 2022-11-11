2372be19a3a159ebb13c1903a176fd48a75ca354
Ze Hua Chen <ze_hua_chen@brown.edu>
2022-11-11 17:02:46 -0500

    feat: complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import "./App.css";
+import BakeryItem from "./components/BakeryItem.js"
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import Grid from '@mui/material/Unstable_Grid2'
+import {Typography} from "@mui/material"
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -10,21 +13,51 @@ bakeryData.forEach((item) => {
 
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
+  const [cart, setCart] = useState({})
   /* add your cart state code here */
+  function handleClick(item) {
+    var number = 1
+    if (item.name in cart) {
+      number = cart[item.name].quantity + 1
+    }
+    setCart({...cart, [item.name]: {quantity: number, price: item.price}})
+  }
 
   return (
-    <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
-
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
-      ))}
-
-      <div>
-        <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
-      </div>
-    </div>
+    <Grid container spacing={6} sx={{px: 15, py: 2}}>
+      <Grid xs={8} spacing={12}>
+        <h1>Will's Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+        <Grid container spacing={4}>
+          {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
+            <Grid key={index} xs={4}>
+              <BakeryItem 
+                item={item}
+                handleClick={handleClick}
+              />
+            </Grid> 
+          ))}
+        </Grid>
+      </Grid>
+      <Grid xs>
+        <h2>My Cart</h2>
+        {Object.keys(cart).map((itemName) => (
+          <Typography mt={2} key={itemName}>
+            {cart[itemName].quantity} x {itemName}
+          </Typography>
+        ))} {/* TODO: render a list of items in the cart */}
+          {Object.keys(cart).length === 0 ? (
+            <Typography fontStyle='italic'>Your cart is currently empty! <br></br> <br></br>  Add your choices by clicking the '+' button</Typography>
+          ) : (
+            <Typography mt={2} variant="h6">
+              Total: ${Object.keys(cart).reduce(
+                (previousTotal, itemName) => (
+                  previousTotal + (cart[itemName].quantity * cart[itemName].price)
+                ), 0
+              )}
+            </Typography>
+          )}
+      </Grid>
+    </Grid>
   );
 }
 
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,62 @@
+import { Button, Card, CardMedia, Typography, Box} from "@mui/material"
+
 // TODO: create a component that displays a single bakery item
+export default function BakeryItem({item, handleClick}) {
+    return (
+        <Card sx={{ width: 1, height: 1 }}>
+            <Box sx={{ display: 'flex', flexDirection: 'column', height: '100%'}}>
+            <CardMedia
+                component='img'
+                image={item.image}
+                alt={item.name}
+            />
+            <Box bgcolor="#e7d2ad"
+            sx={{ 
+                    display: 'flex', 
+                    flexDirection: 'column',
+                    justifyContent: 'space-between',
+                    height: '100%',
+                    gap: '1rem',
+                    padding: '1rem 1rem'
+                }}
+                >
+                    <Box>
+                        <Typography sx={{ 
+                            fontSize: 'h7.fontSize',
+                            fontWeight: 'bold',
+                            color: '#3D1F04',
+                        }}
+                        >
+                            {item.name}
+                        </Typography>
+                        <Typography sx={{
+                            color: '#3D1F04'
+                        }}>
+                            {item.description}
+                        </Typography>
+                    </Box>
+                    <Box sx={{ 
+                        display: 'flex', 
+                        justifyContent: 'space-around',
+                        alignItems: 'center'
+                        }}
+                    >
+                        <Typography
+                            sx={{color: '#3D1F04'}}
+                            >
+                            {'$' + item.price}
+                        </Typography>
+                        <Button 
+                            variant="contained"
+                            color="secondary"
+                            disableElevation
+                            onClick={() => handleClick(item)}
+                        >
+                            +
+                        </Button>
+                    </Box>   
+                </Box>
+            </Box>
+        </Card>
+    )
+}
